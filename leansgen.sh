3f279ec14dd97102efa2746fb016c41775787e08
LeandroTheDev <leandro.schmidt.profissional@gmail.com>
2025-12-09 12:33:21 -0300

    fixed urls

--- leansgen.sh
+++ leansgen.sh
@@ -31,21 +31,21 @@ read -p "Enter your choice: " option
 
 case "$option" in
     1)
-        arch-chroot /mnt bash -c 'export INSTALLPARTITION="$disk" && sh -c "$(curl -sS https://raw.githubusercontent.com/LeandroTheDev/arch_linux/refs/heads/leansgen/Installation/full.sh)"'
+        arch-chroot /mnt bash -c 'export INSTALLPARTITION="$disk" && sh -c "$(curl -sS https://raw.githubusercontent.com/LeandroTheDev/leans_gen/refs/heads/main/Installation/full.sh)"'
         if [ $? -ne 0 ]; then
             echo "The device installation has failed."
             exit 1
         fi
         ;;
     2)
-        arch-chroot /mnt bash -c 'export INSTALLPARTITION="$disk" && sh -c "$(curl -sS https://raw.githubusercontent.com/LeandroTheDev/arch_linux/refs/heads/leansgen/Installation/server.sh)"'
+        arch-chroot /mnt bash -c 'export INSTALLPARTITION="$disk" && sh -c "$(curl -sS https://raw.githubusercontent.com/LeandroTheDev/leans_gen/refs/heads/main/Installation/server.sh)"'
         if [ $? -ne 0 ]; then
             echo "The device installation has failed."
             exit 1
         fi
         ;;
     3)
-        arch-chroot /mnt bash -c 'export INSTALLPARTITION="$disk" && sh -c "$(curl -sS https://raw.githubusercontent.com/LeandroTheDev/arch_linux/refs/heads/leansgen/Installation/empty.sh)"'
+        arch-chroot /mnt bash -c 'export INSTALLPARTITION="$disk" && sh -c "$(curl -sS https://raw.githubusercontent.com/LeandroTheDev/leans_gen/refs/heads/main/Installation/empty.sh)"'
         if [ $? -ne 0 ]; then
             echo "The device installation has failed."
             exit 1
